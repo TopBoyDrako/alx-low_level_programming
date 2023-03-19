95b5aa6e11371fb44c6fd649d73c07ce62158731
TopBoyDrako <sinbadprince9@gmail.com>
2023-03-19 15:26:54 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-int a = '0';
-int b = '0';
+int a = 0;
+int b = 0;
 
-while (a <= '9')
+while (a <= 9)
 {
-while (b <= '9')
+while (b <= 9)
 {
 if (!(a > b) || a == b)
 {
 putchar(a);
 putchar(b);
-if (a == '8' && b == '9')
+if (a == 8 && b == 9)
 {
 	putchar('\n');
 }
@@ -31,7 +31,7 @@ else
 }
 b++;
 }
-b = '0';
+b = 0;
 a++;
 }
 return (0);
