d195c0a3cc8f045c39e425e707f70311d34f5f46
TopBoyDrako <sinbadprince9@gmail.com>
2023-04-17 15:26:48 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This task is on 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef NEW_DOG
+#define NEW_DOG
+
+/**
+ * struct dog - a new type describing a dog
+ * @name: Name of the dog
+ * @age: How old the dog is
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
