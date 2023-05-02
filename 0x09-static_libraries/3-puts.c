6c3d6b4dddb9e86521e908e1929966bb6fd22453
TopBoyDrako <sinbadprince9@gmail.com>
2023-05-02 14:53:28 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void _puts(char *str)
+
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * _puts - entry
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This task is all about 0x09-static_libraries
