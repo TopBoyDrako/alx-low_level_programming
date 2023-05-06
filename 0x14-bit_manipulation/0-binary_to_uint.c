95b81c09b5d18514ec61bc5add45299d8366229b
TopBoyDrako <sinbadprince9@gmail.com>
2023-05-06 14:54:08 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int unit = 0;
 
+	if (!b)
+
+		return (0);	
+
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		unit = 2 * unit + (b[i] - '0');
 	}
-
-	if (!b)
-		return (0);
 
 	return (unit);
 }
