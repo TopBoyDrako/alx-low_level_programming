adefb5fb89342b2315cec25f304fefd514cfa417
TopBoyDrako <sinbadprince9@gmail.com>
2024-01-01 15:27:28 +0100

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module is on a technical interview preparation. Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """ This returns te perimeter of the island"""
+
+    land = 0
+    width = len(gid[0]) - 1
+    length = len(grid) - 1
+
+    for i, j in enumerate(grid):
+        for n, m in enumerate(grid):
+            if m == 1:
+                if i == 0 or grid[i - 1][n] != 1:
+                    land += 1
+                if n ==  0 or grid[i][n - 1] != 1:
+                    land += 1
+                if n == width or grid[i][n + 1] != 1:
+                    land += 1
+                if i == length or grid[i + 1][n] != 1:
+                    land += 1
+    return land
